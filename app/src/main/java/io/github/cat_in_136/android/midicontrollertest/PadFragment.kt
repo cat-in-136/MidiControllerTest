b024502396cfe4d6a455d6ae6f8aa89ea35a1f58
cat_in_136 <cat.in.136+github@gmail.com>
2021-09-23 23:43:50 +0900

    refactor PadFragment.kt to use resources.getIdentifer() instead of ugly array of R.id.buttonX

--- app/src/main/java/io/github/cat_in_136/android/midicontrollertest/PadFragment.kt
+++ app/src/main/java/io/github/cat_in_136/android/midicontrollertest/PadFragment.kt
@@ -24,24 +24,9 @@ class PadFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        arrayOf(
-            R.id.button1,
-            R.id.button2,
-            R.id.button3,
-            R.id.button4,
-            R.id.button5,
-            R.id.button6,
-            R.id.button7,
-            R.id.button8,
-            R.id.button9,
-            R.id.button10,
-            R.id.button11,
-            R.id.button12,
-            R.id.button13,
-            R.id.button14,
-            R.id.button15,
-            R.id.button16
-        ).forEachIndexed { index, btn_id ->
+        val packageName = requireActivity().packageName
+        for (i in 0..15) {
+            val btn_id = resources.getIdentifier("button${i + 1}", "id", packageName)
             val btn = view.findViewById<Button>(btn_id)
             val label = btn!!.text
             val noteNumber = MidiConnection.indexOctaveToNoteNumber(label)
